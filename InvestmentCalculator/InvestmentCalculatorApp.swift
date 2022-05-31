39417b9a6166f70f5f154b2088a2f4e23f23f7ce
Zharkyn Razbek <nisuju44@gmail.com>
2022-05-31 20:18:30 +0600

    Initial Commit

---
+++ InvestmentCalculator/InvestmentCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestmentCalculatorApp.swift
+//  InvestmentCalculator
+//
+//  Created by Zharkyn Razbek on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct InvestmentCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
